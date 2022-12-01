d44d627a3839b3d67fc9cd2b652945a321eae92a
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-12-01 10:09:21 -0600

    day 1 part 2

--- day1/app/Main.hs
+++ day1/app/Main.hs
@@ -1,8 +1,16 @@
 module Main where
 
 import Data.List.Split
+import Data.List
 
+-- part 1
+-- main :: IO ()
+-- main = do
+--     input <- getContents
+--     print $ (maximum . map sum . map (map (\x -> read x :: Integer)) . map lines . splitOn "\n\n") input
+
+-- part 2
 main :: IO ()
 main = do
     input <- getContents
-    print $ (maximum . map sum . map (map (\x -> read x :: Integer)) . map lines . splitOn "\n\n") input
+    print $ (sum . take 3 . reverse . sort . map sum . map (map (\x -> read x :: Integer)) . map lines . splitOn "\n\n") input
