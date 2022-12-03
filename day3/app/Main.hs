974a82b910910f59627efeefc93bf0b9ba91c3d3
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-12-02 23:49:38 -0600

    day 3 part 2

--- day3/app/Main.hs
+++ day3/app/Main.hs
@@ -3,6 +3,24 @@ module Main where
 import qualified Data.Set as S
 import Data.List
 
+-- main :: IO ()
+-- main = do
+--     input <- getContents
+--     print $ (
+--         sum
+--         . map getValue
+--         . concat
+--         . map (\(x,y) -> S.toList $ S.intersection (S.fromList x) (S.fromList y))
+--         . map (\x -> splitAt ((length x) `div` 2) x) 
+--         . lines) input
+
+-- letters = ['_'] ++ ['a'..'z'] ++ ['A'..'Z']
+
+-- getValue :: Char -> Int
+-- getValue c = case elemIndex c letters of
+--     Just x -> x
+--     Nothing -> error (c:" is no good")
+
 main :: IO ()
 main = do
     input <- getContents
@@ -10,8 +28,8 @@ main = do
         sum
         . map getValue
         . concat
-        . map (\(x,y) -> S.toList $ S.intersection (S.fromList x) (S.fromList y))
-        . map (\x -> splitAt ((length x) `div` 2) x) 
+        . map (\(x,y,z) -> S.toList $ S.intersection (S.intersection (S.fromList x) (S.fromList y)) (S.fromList z))
+        . groupBags
         . lines) input
 
 letters = ['_'] ++ ['a'..'z'] ++ ['A'..'Z']
@@ -20,3 +38,7 @@ getValue :: Char -> Int
 getValue c = case elemIndex c letters of
     Just x -> x
     Nothing -> error (c:" is no good")
+
+groupBags :: [String] -> [(String, String, String)]
+groupBags [] = []
+groupBags (x:y:z:xs) = (x, y, z) : groupBags xs
