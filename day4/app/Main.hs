bdbd2f0a4b58f46ec4800338be8f17038b36e593
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-12-04 14:34:29 -0600

    day 4

---
+++ day4/app/Main.hs
@@ -0,0 +1,53 @@
+module Main where
+
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Void
+import Control.Monad
+import Control.Arrow
+import Data.List.Extra
+
+-- main :: IO ()
+-- main = do
+--     input <- getContents
+--     case parse p "" input of
+--         Left bundle -> putStrLn $ errorBundlePretty bundle
+--         Right x -> print $ (
+--             filter (\((x1,x2), (y1,y2)) -> x1 <= y1 && y2 <= x2 || y1 <= x1 && x2 <= y2)
+--             >>> length ) x
+
+-- p :: Parsec Void String [((Int,Int), (Int,Int))]
+-- p = some (do
+--     p1 <- pair <* char ','
+--     p2 <- pair <* newline
+--     return (p1,p2))
+
+-- pair :: Parsec Void String (Int, Int)
+-- pair = do
+--     n1 <- some digitChar
+--     char '-'
+--     n2 <- some digitChar
+--     return (read n1 :: Int, read n2 :: Int)
+
+
+main :: IO ()
+main = do
+    input <- getContents
+    case parse p "" input of
+        Left bundle -> putStrLn $ errorBundlePretty bundle
+        Right x -> print $ (
+            filter (\((x1,x2), (y1,y2)) -> notNull $ intersect [x1..x2] [y1..y2])
+            >>> length ) x
+
+p :: Parsec Void String [((Int,Int), (Int,Int))]
+p = some (do
+    p1 <- pair <* char ','
+    p2 <- pair <* newline
+    return (p1,p2))
+
+pair :: Parsec Void String (Int, Int)
+pair = do
+    n1 <- some digitChar
+    char '-'
+    n2 <- some digitChar
+    return (read n1 :: Int, read n2 :: Int)
