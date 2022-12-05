7809335ff17302bc6509c4abc68b54872b871c1a
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-12-05 15:32:44 -0600

    refactored Day3.hs

--- Day3.hs
+++ Day3.hs
@@ -1,36 +1,27 @@
-module Main where
-
 import qualified Data.Set as S
 import Data.List
-
--- main :: IO ()
--- main = do
---     input <- getContents
---     print $ (
---         sum
---         . map getValue
---         . concat
---         . map (\(x,y) -> S.toList $ S.intersection (S.fromList x) (S.fromList y))
---         . map (\x -> splitAt ((length x) `div` 2) x) 
---         . lines) input
-
--- letters = ['_'] ++ ['a'..'z'] ++ ['A'..'Z']
-
--- getValue :: Char -> Int
--- getValue c = case elemIndex c letters of
---     Just x -> x
---     Nothing -> error (c:" is no good")
 
 main :: IO ()
 main = do
     input <- getContents
-    print $ (
-        sum
-        . map getValue
-        . concat
-        . map (\(x,y,z) -> S.toList $ S.intersection (S.intersection (S.fromList x) (S.fromList y)) (S.fromList z))
-        . groupBags
-        . lines) input
+    print $ solve input
+    print $ solve' input
+
+solve' :: String -> Int
+solve' = sum
+    . map getValue
+    . concat
+    . map (\(x,y,z) -> S.toList $ S.intersection (S.intersection (S.fromList x) (S.fromList y)) (S.fromList z))
+    . groupBags
+    . lines
+
+solve :: String -> Int
+solve = sum
+    . map getValue
+    . concat
+    . map (\(x,y) -> S.toList $ S.intersection (S.fromList x) (S.fromList y))
+    . map (\x -> splitAt ((length x) `div` 2) x) 
+    . lines
 
 letters = ['_'] ++ ['a'..'z'] ++ ['A'..'Z']
 
