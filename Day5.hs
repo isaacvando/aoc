54c897339c00bdc1a71d07a2fa8651cd5f956741
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-12-05 18:16:01 -0600

    refactored Day5

--- Day5.hs
+++ Day5.hs
@@ -1,41 +1,16 @@
-module Main where
-
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Data.Void
 import Data.List
 import Data.Char
 
--- main :: IO ()
--- main = do
---     input <- fmap lines getContents
---     let crates = getCrates input
---     let inst = map getInst (drop 10 input)
---     putStrLn $ map head $ foldl' (\acc [x,y,z] -> move x y z acc) crates inst
-
--- getCrates :: [String] -> [String]
--- getCrates = map (filter (/= ' ')) . transpose . map parseCrates . take 8
-
--- parseCrates :: String -> String
--- parseCrates (_:c:_:_:xs) = c : parseCrates xs
--- parseCrates (_:c:_:[]) = [c]
-
--- getInst :: String -> [Int]
--- getInst = map read . filter (all isDigit) . words
-
--- move :: Int -> Int -> Int -> [String] -> [String]
--- move 0 _ _ crates = crates
--- move count source target crates = move (count-1) source target $
---     let (xs,y:ys) = splitAt (source-1) crates 
---         in (let (as, b:bs) = splitAt (target-1) (xs ++ [tail y] ++ ys) 
---                 in as ++ [(head y):b] ++ bs)
-
 main :: IO ()
 main = do
-    input <- fmap lines getContents
+    input <- fmap lines (readFile "5.txt")
     let crates = getCrates input
     let inst = map getInst (drop 10 input)
     putStrLn $ map head $ foldl' (\acc [x,y,z] -> move x y z acc) crates inst
+    putStrLn $ map head $ foldl' (\acc [x,y,z] -> move' x y z acc) crates inst
 
 getCrates :: [String] -> [String]
 getCrates = map (filter (/= ' ')) . transpose . map parseCrates . take 8
@@ -48,7 +23,14 @@ getInst :: String -> [Int]
 getInst = map read . filter (all isDigit) . words
 
 move :: Int -> Int -> Int -> [String] -> [String]
-move count source target crates =
+move 0 _ _ crates = crates
+move count source target crates = move (count-1) source target $
+    let (xs,y:ys) = splitAt (source-1) crates 
+        in (let (as, b:bs) = splitAt (target-1) (xs ++ [tail y] ++ ys) 
+                in as ++ [(head y):b] ++ bs)
+
+move' :: Int -> Int -> Int -> [String] -> [String]
+move' count source target crates =
     let (xs,y:ys) = splitAt (source-1) crates 
         in (let (as, b:bs) = splitAt (target-1) (xs ++ [drop count y] ++ ys) 
                 in as ++ [chunk ++ b] ++ bs)
