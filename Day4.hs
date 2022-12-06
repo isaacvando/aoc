49b6dc57134fdb200e84a743f9d6bd9a109f8b0f
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-12-05 18:12:03 -0600

    refactored Day4

--- Day4.hs
+++ Day4.hs
@@ -5,36 +5,14 @@ import Control.Monad
 import Control.Arrow
 import Data.List.Extra
 
--- main :: IO ()
--- main = do
---     input <- getContents
---     case parse p "" input of
---         Left bundle -> putStrLn $ errorBundlePretty bundle
---         Right x -> print $ (
---             filter (\((x1,x2), (y1,y2)) -> x1 <= y1 && y2 <= x2 || y1 <= x1 && x2 <= y2)
---             >>> length ) x
-
--- p :: Parsec Void String [((Int,Int), (Int,Int))]
--- p = some (do
---     p1 <- pair <* char ','
---     p2 <- pair <* newline
---     return (p1,p2))
-
--- pair :: Parsec Void String (Int, Int)
--- pair = do
---     n1 <- some digitChar
---     char '-'
---     n2 <- some digitChar
---     return (read n1 :: Int, read n2 :: Int)
-
 main :: IO ()
 main = do
-    input <- getContents
+    input <- readFile "4.txt"
     case parse p "" input of
         Left bundle -> putStrLn $ errorBundlePretty bundle
-        Right x -> print $ (
-            filter (\((x1,x2), (y1,y2)) -> notNull $ intersect [x1..x2] [y1..y2])
-            >>> length ) x
+        Right x -> do
+            print $ (length . filter (\((x1,x2), (y1,y2)) -> x1 <= y1 && y2 <= x2 || y1 <= x1 && x2 <= y2)) x
+            print $ (length . filter (\((x1,x2), (y1,y2)) -> notNull $ intersect [x1..x2] [y1..y2])) x
 
 p :: Parsec Void String [((Int,Int), (Int,Int))]
 p = some (do
@@ -47,4 +25,4 @@ pair = do
     n1 <- some digitChar
     char '-'
     n2 <- some digitChar
-    return (read n1 :: Int, read n2 :: Int)
+    return (read n1, read n2)
