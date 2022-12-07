a228c90d46efbbe124a472430a9769ef89e58236
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-12-07 09:31:38 -0600

    day 7

--- Day7.hs
+++ Day7.hs
@@ -8,10 +8,13 @@ type Parser = Parsec Void String
 
 main :: IO ()
 main = do
-    input <- readFile "foo.txt"
+    input <- readFile "7.txt"
     case parse dir "" input of
         Left x -> putStrLn $ errorBundlePretty x 
-        Right x -> print x
+        Right x -> do
+            print $ (sum . filter (<= 100000) . listSizes) x
+            print $ (minimum . filter (\x -> availableSpace + x >= 30000000) . listSizes) x
+                where availableSpace = 70000000 - maximum (listSizes x)
 
 dir :: Parser Tree
 dir = do
@@ -27,3 +30,11 @@ file = do
 
 dirName :: Parser Tree
 dirName = return Name <* string "dir " <* some (noneOf "\n") <* newline
+
+getSize :: Tree -> Int
+getSize (File n) = n
+getSize (Dir _ trees) = foldr (\x acc -> getSize x + acc) 0 trees
+
+listSizes :: Tree -> [Int]
+listSizes t@(Dir _ trees) = getSize t : (trees >>= listSizes)
+listSizes _ = []
