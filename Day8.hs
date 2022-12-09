22e01e43f6e7c69b893582c4976c4c939b395dcb
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-12-08 21:07:53 -0600

    refactored Day8

--- Day8.hs
+++ Day8.hs
@@ -1,34 +1,27 @@
 import Data.Char
 import qualified Data.Map as Map
+import Data.Bifunctor
 
 type Pos = (Int,Int)
 
 main :: IO ()
 main = do
     input <- readFile "input/8.txt"
-    let rowCount = length (lines input)
-    let columnCount = length ((lines input) !! 0)
     let trees = (getMap . map (map digitToInt) . lines) input
-    print $ (foldr (\(_,x) acc -> acc + x) 0 . Map.toList . Map.mapWithKey (f trees rowCount columnCount)) trees
-    print $ (maximum . map product . map snd . Map.toList . Map.mapWithKey (f' trees rowCount columnCount)) trees
+    print $ (length . filter id . map (or . snd) . Map.toList . Map.mapWithKey (\k v -> map (comp trees k v) moves)) trees
+    print $ (maximum . map (product . snd) . Map.toList . Map.mapWithKey (\k v -> map (count trees k v) moves)) trees
         where 
-            f trees rowCount colCount (row, col) val = if all (\x -> comp (row - x, col) trees val) [1..rowCount] 
-                    || all (\x -> comp (row + x, col) trees val) [1..rowCount] 
-                    || all (\x -> comp (row, col - x) trees val) [1..colCount]
-                    || all (\x -> comp (row, col + x) trees val) [1..colCount]
-                    then 1 else 0
-            f' trees rowCount colCount position val = 
-                map (count position trees val) [\(x,y) -> (x-1, y), \(x,y) -> (x+1, y), \(x,y) -> (x, y-1), \(x,y) -> (x, y+1)]
+            moves = [first (+1), first (+(-1)), second (+1), second (+(-1))]
 
-comp :: Pos -> Map.Map Pos Int -> Int -> Bool
-comp (x,y) trees val = case Map.lookup (x,y) trees of
+comp :: Map.Map Pos Int -> Pos -> Int -> (Pos -> Pos) -> Bool
+comp trees position val move = let pos = move position in case Map.lookup pos trees of
     Nothing -> True
-    Just x -> x < val
+    Just x -> (x < val) && comp trees pos val move
 
-count :: Pos -> Map.Map Pos Int -> Int -> (Pos -> Pos) -> Int
-count position trees val getPos = let pos = getPos position in case Map.lookup (getPos position) trees of
+count :: Map.Map Pos Int -> Pos -> Int -> (Pos -> Pos) -> Int
+count trees position val move = let pos = move position in case Map.lookup pos trees of
     Nothing -> 0
-    Just x -> if x >= val then 1 else 1 + count pos trees val getPos
+    Just x -> if x >= val then 1 else 1 + count trees pos val move
 
 getMap :: [[Int]] -> Map.Map Pos Int
-getMap xs = Map.fromList $ concat [[ ((row,col), (xs !! row) !! col) | col <- [0..length (xs !! 0) - 1]] | row <- [0..length xs - 1]]
+getMap xs = Map.fromList $ concat [[ ((row,col), (xs !! row) !! col) | col <- [0..length (head xs) - 1]] | row <- [0..length xs - 1]]
