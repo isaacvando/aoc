1ce26bd77b7decd8a36cbd8508cf2bbf67cf3347
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-12-09 23:38:51 -0600

    Day 10 part 2

--- Day10.hs
+++ Day10.hs
@@ -1,13 +1,15 @@
+-- This is such a fun problem!!
 import Data.List
+import Data.List.Split
 
 main :: IO ()
 main = do
     input <- readFile "input/10.txt"
-    let insts = id : (concat . map (getInst . words) . lines) input -- appended id for 1 based indexing
+    let insts = lines input >>= (getInst . words)
     let states = scanl' (\acc f -> f acc) 1 insts
-    print $ sum $ map (\x -> (states !! x) * x) [20,60..220]
+    print $ sum $ map (\x -> (states !! x) * (x+1)) [19,59..219]
+    mapM_ putStrLn (chunksOf 40 $ map (\x -> if abs ((states !! x) - (x `mod` 40)) <= 1 then '#' else ' ') [0..40*6 - 1])
     
-
 getInst :: [String] -> [Int -> Int]
 getInst ["addx", n] = [id, (+ read n)]
 getInst ["noop"] = [id]
