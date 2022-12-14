9c84d8e327a639da39f3d318c3a2182e92aa8d38
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-12-14 10:43:37 -0600

    day 13

--- Day13.hs
+++ Day13.hs
@@ -1,16 +1,55 @@
-module Day13 where
-
+import Data.List
 import Data.List.Split
+import Data.Char
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Void
+import Data.Maybe
+
+type Parser = Parsec Void String
+
+data Quality = Good | Bad | Idk deriving (Eq,Show)
+
+newtype Packet = Packet String deriving (Eq,Show)
+
+instance Ord Packet where
+    compare (Packet p) (Packet q) = if check [p] [q] == Good then GT else LT
 
 main :: IO ()
 main = do
-    input <- readFile "foo.txt"
+    input <- readFile "input/13.txt"
     let pairs = (splitOn [""] . lines) input
-    print pairs
+    let results = map (\[x,y] -> check [x] [y]) pairs
+    let packets = [Packet "[[2]]",Packet "[[6]]"] ++ concatMap (map Packet) pairs
+    print $ sum $ zipWith (\b n -> if b == Good then n else 0) results [1..]
+    let getIndex p = fromJust (elemIndex p (reverse (sort packets))) + 1
+    print $ getIndex (Packet "[[2]]") * getIndex (Packet "[[6]]")
 
-compare :: String -> String -> Bool
-compare p1 p2 = False
+check :: [String] -> [String] -> Quality
+check [] [] = Idk
+check _ [] = Bad
+check [] _ = Good
+check (l:ls) (r:rs)
+    | l == r = check ls rs
+    | all isDigit l && all isDigit r = if (read l :: Int) < read r then Good else Bad
+    | otherwise = case check (separate l) (separate r) of
+        Idk -> check ls rs
+        x -> x
 
-peel :: String -> String
-peel xs@('[':y:_) = (init . tail) xs
-peel x = x
+separate :: String -> [String]
+separate xs = case parse chunks "" (peel xs) of
+    Left e -> error $ errorBundlePretty e
+    Right x -> x
+    where
+        peel xs@('[':y:_) = (init . tail) xs
+        peel x = x
+
+chunks :: Parser [String]
+chunks = many ((some digitChar <|> list) <* optional (char ','))
+
+list :: Parser String
+list = do
+    char '['
+    xs <- many ((some digitChar <|> list) <* optional (char ','))
+    char ']'
+    return $ "[" ++ intercalate "," xs ++ "]"
--- Day13Spec.hs
+++
@@ -1,9 +0,0 @@
-import Test.Hspec
-
-import Day13 hiding (main)
-
-main :: IO ()
-main = hspec $ do
-        describe "tests" $ do
-            it "should peel" $ do
-                peel "[foo]" `shouldBe` ""
