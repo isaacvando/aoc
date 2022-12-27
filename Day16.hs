433458259e9037f7469e4f165bce59c05737bfb8
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-12-26 22:42:42 -0500

    Day 16 part 1 and wip part 2

--- Day16.hs
+++ Day16.hs
@@ -2,6 +2,7 @@ import Text.Megaparsec
 import Text.Megaparsec.Char
 import Data.Void
 import qualified Data.Map as M
+import qualified Data.Set as S
 import Data.Maybe
 import Data.List
 
@@ -11,12 +12,14 @@ type Graph = M.Map String (Int,[String])
 
 main :: IO ()
 main = do
-    -- input <- readFile "foo.txt"
     input <- readFile "input/16.txt"
     let valves = map getValve (lines input)
     let graph = M.fromList valves
     let paths = foldl' (\acc _ -> refine graph (concatMap (steps graph) acc)) [[Goto "AA"]] [1..30]
     print $ getValue graph (getMax graph paths)
+
+    let paths' = foldl' (\acc _ -> refine' graph (concatMap (steps graph) acc)) [[Goto "AA"]] [1..26]
+    print $ getValue' graph (getMax' graph (pairs paths'))
 
 refine :: Graph -> [Path] -> [Path]
 refine graph paths = foldr (\x acc -> (getMax graph (collect x)) : acc) [] symbols
@@ -24,11 +27,24 @@ refine graph paths = foldr (\x acc -> (getMax graph (collect x)) : acc) [] symbo
         symbols = nub (map head paths)
         collect symb = filter (\x -> head x == symb) paths
 
+refine' :: Graph -> [Path] -> [Path]
+refine' graph p = unpairs $ foldr (\x acc -> (getMax' graph (collect x)) : acc) [] symbols
+    where
+        paths = pairs p
+        symbols = pairs $ nub (map head p)
+        collect symb = filter (\(x,y) -> (head x, head y) == symb) paths
+
 getMax :: Graph -> [Path] -> Path
 getMax graph paths = f paths (-1) []
     where 
         f [] _ result = result
         f (x:xs) n result = let v = getValue graph x in if v > n then f xs v x else f xs n result
+
+getMax' :: Graph -> [(Path,Path)] -> (Path,Path)
+getMax' graph paths = f paths (-1) ([],[])
+    where 
+        f [] _ result = result
+        f (x:xs) n result = let v = getValue' graph x in if v > n then f xs v x else f xs n result
 
 getValue :: Graph -> Path -> Int
 getValue graph path = f (reverse path) 0 0
@@ -39,8 +55,19 @@ getValue graph path = f (reverse path) 0 0
             Open loc -> f xs (total + (flow * (30 - n))) (n+1)
                 where flow = fst (fromJust (M.lookup loc graph))
 
+getValue' :: Graph -> (Path, Path) -> Int
+getValue' graph (p1, p2) = f (reverse p1) (reverse p2) M.empty 4
+    where
+        f [] [] open _ = M.foldr (+) 0 open
+        f (x:xs) (y:ys) open n = f xs ys (put x (put y open)) (n+1)
+            where
+                put (Goto _) m = m
+                put (Open name) m = let flow = fst (fromJust (M.lookup name graph)) * (30 - n) in
+                    case M.lookup name m of
+                        Nothing -> M.insert name flow m
+                        Just val -> if flow > val then M.insert name flow m else m
+
 steps :: Graph -> Path -> [Path]
-steps _ [] = error "wtf"
 steps graph (x:xs) = filter valid (map (:x:xs) moves)
     where 
         valid path = (opens path) == nub (opens path)
@@ -49,6 +76,12 @@ steps graph (x:xs) = filter valid (map (:x:xs) moves)
         moves = case x of
             Open name -> map Goto (neighs name) ++ [Goto name]
             Goto name -> map Goto (neighs name) ++ [Open name, Goto name]
+
+pairs :: [a] -> [(a,a)]
+pairs a = (\x y -> (x,y)) <$> a <*> a
+
+unpairs :: Eq a => [(a,a)] -> [a]
+unpairs a = nub $ concatMap (\(x,y) -> [x,y]) a
 
 isOpen :: Action -> Bool
 isOpen (Open _) = True
